c60e8381e054a59638bd82e5dbf67f5d7528b275
Mildness Onyekwere <mno5865@rit.edu>
2022-10-18 13:24:40 -0400

    i hope this works this time

--- pdmapi/src/main/java/com/example/pdmapi/Model/User.java
+++ pdmapi/src/main/java/com/example/pdmapi/Model/User.java
@@ -1,2 +1,35 @@
-package com.example.pdmapi.Model;public class User {
+package com.example.pdmapi.Model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "User")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "UserID")
+    private long userID;
+
+    @Column(name = "Username")
+    private String username;
+
+    @Column(name = "Password")
+    private String password;
+
+    @Column(name = "Email")
+    private String email;
+
+    @Column(name = "FirstName")
+    private String firstName;
+
+    @Column(name = "LastName")
+    private String lastName;
+
+    @Column(name = "CreationDate")
+    private String creationDate;
+
+    @Column(name = "AccessDate")
+    private String accessDate;
+
 }
