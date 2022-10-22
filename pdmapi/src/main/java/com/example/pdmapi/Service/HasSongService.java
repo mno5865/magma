c63dd7a69ed42614f4f0738dfaa310d87994162b
mpb8984 <71938683+mpb8984@users.noreply.github.com>
2022-10-22 15:29:53 -0400

    Finished testing

--- pdmapi/src/main/java/com/example/pdmapi/Service/HasSongService.java
+++ pdmapi/src/main/java/com/example/pdmapi/Service/HasSongService.java
@@ -43,6 +43,7 @@ public class HasSongService {
         hasSong.setId(songGenreKey);
         hasSong.setGenre(genre);
         hasSong.setSong(song);
+        hasSongRepository.delete(hasSong);
     }
 
     public List<HasSong> getSongGenres(long songId)
