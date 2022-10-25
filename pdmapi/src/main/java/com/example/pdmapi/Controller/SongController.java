407fbcce7235ff86e269812a6d7d1435461a4e3b
mpb8984 <71938683+mpb8984@users.noreply.github.com>
2022-10-25 11:47:08 -0400

    song controller

--- pdmapi/src/main/java/com/example/pdmapi/Controller/SongController.java
+++ pdmapi/src/main/java/com/example/pdmapi/Controller/SongController.java
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,14 +20,22 @@ public class SongController {
 
     @GetMapping("/songs")
     public ResponseEntity<List<Song>> getSongs() {
-        return new ResponseEntity<>(songService.getSongs(), HttpStatus.OK);
+        List<Song> songs = songService.getSongs();
+        if(songs == null)
+        {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        else
+        {
+            return new ResponseEntity<>(songService.getSongs(), HttpStatus.OK);
+        }
     }
 
     @GetMapping("/songs/{id}")
     public ResponseEntity<Song> getSong(@PathVariable long id) {
-        Optional<Song> song = songService.getSong(id);
-        if (song.isPresent()) {
-            return new ResponseEntity<>(song.get(), HttpStatus.OK);
+        Song song = songService.getSong(id);
+        if (song != null) {
+            return new ResponseEntity<>(song, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
@@ -34,20 +43,19 @@ public class SongController {
 
     @PostMapping(value = "/songs", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Song> createSong(@RequestBody Song newSong) {
-        Song song = songService.createSong(newSong);
-
-        if (song == null) {
+        int rowsAffected = songService.createSong(newSong);
+        if (rowsAffected == -1) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         } else {
-            return new ResponseEntity<>(song, HttpStatus.CREATED);
+            return new ResponseEntity<>(HttpStatus.CREATED);
         }
     }
 
     @PutMapping(value = "/songs/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Song> updateSong(@PathVariable long id, @RequestBody Song songDetails) {
-        Optional<Song> song = songService.getSong(id);
-        if (song.isPresent()) {
-            return new ResponseEntity<>(songService.updateSong(id, songDetails), HttpStatus.OK);
+    public ResponseEntity updateSong(@PathVariable long id, @RequestBody Song songDetails) {
+        int rowsAffected = songService.updateSong(id, songDetails);
+        if (rowsAffected != -1) {
+            return new ResponseEntity<>(HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
@@ -55,8 +63,16 @@ public class SongController {
 
     @DeleteMapping("/songs/{id}")
     public ResponseEntity deleteSong(@PathVariable long id) {
-        songService.deleteSong(id);
-        return new ResponseEntity<>(HttpStatus.OK);
+        int rowsAffected = songService.deleteSong(id);
+        if(rowsAffected != -1)
+        {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else
+        {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
     }
 
 }
