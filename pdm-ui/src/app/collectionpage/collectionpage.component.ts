a811dbba4093616518257e225897eb49286548db
Adrian Burgos <awb8593@rit.edu>
2022-10-26 00:45:04 -0400

    created Collecctionpage component to display songs for an individual collection

---
+++ pdm-ui/src/app/collectionpage/collectionpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collectionpage',
+  templateUrl: './collectionpage.component.html',
+  styleUrls: ['./collectionpage.component.css']
+})
+export class CollectionpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
