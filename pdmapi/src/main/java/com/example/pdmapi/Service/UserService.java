b731164156413452cff4dfac4c62f9cf4affe6e5
Mildness Onyekwere <mno5865@rit.edu>
2022-10-26 03:43:54 -0400

    progress update

--- pdmapi/src/main/java/com/example/pdmapi/Service/UserService.java
+++ pdmapi/src/main/java/com/example/pdmapi/Service/UserService.java
@@ -13,12 +13,14 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Base64;
 
 @Service
 public class UserService {
     @Autowired
     DataSource dataSource;
 
+    // CREATE
     public int createUser(User user) {
         String stmt = ("INSERT INTO \"user\"(email, username, password, first_name, last_name, creation_date, " +
                 "access_date) VALUES('%s', '%s', '%s', '%s', '%s', '%tF', '%tc')").formatted(user.getEmail(),
@@ -36,6 +38,37 @@ public class UserService {
         return -1;
     }
 
+    public int createUserCreatesCollection(long userId, long collectionId) {
+        String st = ("INSERT INTO user_creates_collection (user_id, collection_id) VALUES (%d, %d)").formatted(userId,
+                collectionId);
+        try {
+            Connection conn = DataSourceUtils.getConnection(dataSource);
+            Statement stmt = conn.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+            return stmt.executeUpdate(st);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public int createUserFriendsUser(long user1Id, long user2Id) {
+        String st = ("INSERT INTO user_friends_user(user_one_id, user_two_id) VALUES (%d, %d)").
+                formatted(user1Id, user2Id);
+        try {
+            Connection conn = DataSourceUtils.getConnection(dataSource);
+            Statement stmt = conn.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+            return stmt.executeUpdate(st);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    // READ
     public User getUser(Long userID) {
         String stmt = "SELECT * FROM \"user\" WHERE user_id=%d".formatted(userID);
         try {
@@ -88,7 +121,10 @@ public class UserService {
         return null;
     }
 
-    public User getUserByEmail(String email) {
+    public User getUserByEmail(String emailHex) {
+        byte[] decodedBytes = Base64.getDecoder().decode(emailHex);
+        String email = new String(decodedBytes);
+
         String stmt = "SELECT * FROM \"user\" WHERE email='%s'".formatted(email);
         try {
             Connection conn = DataSourceUtils.getConnection(dataSource);
@@ -112,53 +148,6 @@ public class UserService {
             e.printStackTrace();
         }
         return null;
-    }
-
-    public int updateUser(Long userId, User user) {
-        String stmt = ("UPDATE \"user\" SET " +
-                "username='%s', password='%s', email='%s', first_name='%s', last_name='%s', creation_date='%tF'," +
-                "access_date='%tc' WHERE user_id=%d").formatted(user.getUsername(), user.getPassword(), user.getEmail(),
-                user.getFirstName(), user.getLastName(), user.getCreationDate(), user.getAccessDate(), userId);
-        try {
-            Connection conn = DataSourceUtils.getConnection(dataSource);
-            Statement statement = conn.createStatement(
-                    ResultSet.TYPE_SCROLL_INSENSITIVE,
-                    ResultSet.CONCUR_UPDATABLE);
-            return statement.executeUpdate(stmt);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return -1;
-    }
-
-    public int deleteUser(Long userId) {
-        String stmt = "DELETE FROM \"user\" WHERE user_id=%d".formatted(userId);
-        try {
-            Connection conn = DataSourceUtils.getConnection(dataSource);
-            Statement statement = conn.createStatement(
-                    ResultSet.TYPE_SCROLL_INSENSITIVE,
-                    ResultSet.CONCUR_UPDATABLE);
-            return statement.executeUpdate(stmt);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return -1;
-    }
-
-    //user_creates_collection RELATIONSHIP
-    public int createUserCreatesCollection(long userId, long collectionId) {
-        String st = ("INSERT INTO user_creates_collection (user_id, collection_id) VALUES (%d, %d)").formatted(userId,
-                collectionId);
-        try {
-            Connection conn = DataSourceUtils.getConnection(dataSource);
-            Statement stmt = conn.createStatement(
-                    ResultSet.TYPE_SCROLL_INSENSITIVE,
-                    ResultSet.CONCUR_UPDATABLE);
-            return stmt.executeUpdate(st);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return -1;
-        }
     }
 
     public List<Collection> getCollectionsByUserID(long userId) {
@@ -188,9 +177,86 @@ public class UserService {
         return collections;
     }
 
-    public int deleteUserCreatesCollection(long userId, long collectionId){
+    public List<User> getFriendsByUser(long userId) {
+        List<users> friends = new ArrayList<>();
+
+        String query = ("SELECT user_id FROM \"user\" "
+                + "INNER JOIN \"user\" on user_friends_user.user_id = \"user\".user_id "
+                + "INNER JOIN collection on user_creates_collection.collection_id = collection.collection_id "
+                + "WHERE \"user\".user_id=%d").formatted(userId);
+        try {
+            Connection conn = DataSourceUtils.getConnection(dataSource);
+            Statement stmt = conn.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = stmt.executeQuery(query);
+
+            while(rs.next()) {
+                Collection collection = new Collection();
+                collection.setCollectionID(rs.getLong("collection_id"));
+                collection.setTitle(rs.getString("title"));
+                collections.add(collection);
+            }
+        }  catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return collections;
+    }
+
+
+
+    // UPDATE
+
+    public int updateUser(Long userId, User user) {
+        String stmt = ("UPDATE \"user\" SET " +
+                "username='%s', password='%s', email='%s', first_name='%s', last_name='%s', creation_date='%tF'," +
+                "access_date='%tc' WHERE user_id=%d").formatted(user.getUsername(), user.getPassword(), user.getEmail(),
+                user.getFirstName(), user.getLastName(), user.getCreationDate(), user.getAccessDate(), userId);
+        try {
+            Connection conn = DataSourceUtils.getConnection(dataSource);
+            Statement statement = conn.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+            return statement.executeUpdate(stmt);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    // DELETE
+    public int deleteUser(Long userId) {
+        String stmt = "DELETE FROM \"user\" WHERE user_id=%d".formatted(userId);
+        try {
+            Connection conn = DataSourceUtils.getConnection(dataSource);
+            Statement statement = conn.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+            return statement.executeUpdate(stmt);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    public int deleteUserCreatesCollection(long userId, long collectionId) {
         String st = ("DELETE FROM user_creates_collection WHERE (user_id=%d AND collection_id=%d)")
                 .formatted(userId, collectionId);
+        try {
+            Connection conn = DataSourceUtils.getConnection(dataSource);
+            Statement stmt = conn.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+            return stmt.executeUpdate(st);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public int deleteUserFriendsUser(long user1Id, long user2Id) {
+        String st = ("DELETE FROM user_friends_user WHERE (user_one_id=%d AND user_two_id=%d)")
+                .formatted(user1Id, user2Id);
         try {
             Connection conn = DataSourceUtils.getConnection(dataSource);
             Statement stmt = conn.createStatement(
