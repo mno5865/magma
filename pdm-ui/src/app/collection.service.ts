affac83a5e46cba99916ea73866940a597d1377f
Adrian Burgos <awb8593@rit.edu>
2022-10-26 13:08:10 -0400

    changed CollectionRedirect Service to use CollectionID instead of Username

--- pdm-ui/src/app/collection.service.ts
+++ pdm-ui/src/app/collection.service.ts
@@ -12,7 +12,18 @@ export class CollectionService {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
+  private collectionID = 0
+  private collectionName = "_"
+
   constructor(private http: HttpClient) { }
+
+  public getCollectionID(): number {
+    return this.collectionID
+  }
+
+  public getCollectionName(): string {
+    return this.collectionName
+  }
 
   createCollection(collection: Collection): Observable<Collection> {
     return this.http.post<Collection>(this.collectionURL, JSON.stringify(collection), this.httpOptions)
--- pdm-ui/src/app/collectionredirect.service.ts
+++ pdm-ui/src/app/collectionredirect.service.ts
@@ -2,20 +2,22 @@ import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from './User';
+import { Collection } from './Collection'
 import { Observable, of } from 'rxjs';
-import { UtilsService } from './utils.service'
+import { CollectionService } from './collection.service'
 import { Router } from '@angular/router';
 
+// This service redirects to a specified Collection owned by the User
 @Injectable({
   providedIn: 'root'
 })
 export class CollectionredirectService implements CanActivate{
 
-  constructor(private router: Router, private utilsService : UtilsService) { }
+  constructor(private router: Router, private collectionService : CollectionService) { }
 
   canActivate(): Observable<boolean> {
-    var username: string = this.utilsService.getUsername();
-    this.router.navigate(["collections/" + username])
+    var collectionID: number = this.collectionService.getCollectionID();
+    this.router.navigate(["collections/" + collectionID])
     return of(false)
   }
 }
