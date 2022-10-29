d06e1d67bcd6d43eddbf92a620c3cc005754b49b
Your <am4063@rit.edu>
2022-10-28 20:36:00 -0400

    AlbumService.java Documented!

--- pdmapi/src/main/java/com/example/pdmapi/Service/AlbumService.java
+++ pdmapi/src/main/java/com/example/pdmapi/Service/AlbumService.java
@@ -36,22 +36,23 @@ public class AlbumService {
      * @param dataSource connects users to the data for albums.
      */
     public AlbumService(DataSource dataSource) {
+
         this.dataSource = dataSource;
     }
 
     // CREATE
 
     /**
-     *
-     * @param album
-     * @return
+     * Creates an album in the database
+     * @param album an album in the database
+     * @return -1
      */
     public int createAlbum(Album album) {
-        //
+        // String that prints a message to insert an album
         String st = ("INSERT INTO album(title, release_date) VALUES ('%s', '%tF')")
                 .formatted(album.getTitle(), album.getReleaseDate());
         Connection conn = DataSourceUtils.getConnection(dataSource);
-        //
+        // Try Catch Error Checking if an album can be created
         try {
             Statement stmt = conn.createStatement(
                     ResultSet.TYPE_SCROLL_INSENSITIVE,
@@ -70,18 +71,18 @@ public class AlbumService {
     }
 
     /**
-     *
-     * @param albumId
-     * @param songId
-     * @return
+     * Makes an album in the database with a song in album.
+     * @param albumId ID of the album
+     * @param songId ID of the song
+     * @return -1
      */
     public int createAlbumContainsSong(long albumId, long songId){
-        //
+        // String that prints a message to insert a song in album
         String st = ("INSERT INTO album_contains_song(album_id, song_id) VALUES (%d, %d)")
                 .formatted(albumId, songId);
         Connection conn = DataSourceUtils.getConnection(dataSource);
 
-        //
+        // Try Catch Error Checking if song can be contained in album
         try {
             Statement stmt = conn.createStatement(
                     ResultSet.TYPE_SCROLL_INSENSITIVE,
@@ -102,21 +103,23 @@ public class AlbumService {
     // READ
 
     /**
-     *
-     * @return
+     * Gets all the Albums in the Database.
+     * @return list of Albums
      */
     public List<Album> getAlbums() {
-        //
+        //// String that Selects an Album
         String query = "SELECT * FROM album";
         Connection conn = DataSourceUtils.getConnection(dataSource);
 
-        //
+        // Try Catch Error Checking album can be obtained
         try {
             Statement stmt = conn.createStatement(
                     ResultSet.TYPE_SCROLL_INSENSITIVE,
                     ResultSet.CONCUR_UPDATABLE);
             ResultSet rs = stmt.executeQuery(query);
             List<Album> albums = new ArrayList<>();
+
+            // If the Album does not exist create the album and then add it to the list of albums
             while(rs.next()) {
                 Album album = new Album();
                 album.setAlbumID(rs.getLong("album_id"));
@@ -138,22 +141,24 @@ public class AlbumService {
     }
 
     /**
-     *
-     * @param albumId
-     * @return
+     * Gets a specfic album from the database.
+     * @param albumId ID of Album
+     * @return the specific album the user is trying to get.
      */
     public Album getAlbum(long albumId) {
-        //
+        // String that prints a message to get the album from a list of albums
         String query = ("SELECT * FROM album WHERE album_id=%d").formatted(albumId);
         Connection conn = DataSourceUtils.getConnection(dataSource);
 
-        //
+        // Try Catch Error Checking if song can be contained in album
         try {
             Statement stmt = conn.createStatement(
                     ResultSet.TYPE_SCROLL_INSENSITIVE,
                     ResultSet.CONCUR_UPDATABLE);
             ResultSet rs = stmt.executeQuery(query);
             Album album = new Album();
+
+            //Sets an albums title,ID and release date
             while(rs.next()) {
                 album.setAlbumID(rs.getLong("album_id"));
                 album.setTitle(rs.getString("title"));
@@ -173,15 +178,15 @@ public class AlbumService {
     }
 
     /**
-     *
-     * @param albumId
-     * @return
+     * Gets the list of songs in a specfic Album in a database.
+     * @param albumId ID of the album
+     * @return a list of Songs in Album
      */
     public List<Song> getSongsByAlbum(long albumId) {
-        //
+        //Creates a list of songs
         List<Song> songs = new ArrayList<>();
 
-        //
+        //String that prints a message to get songs in an album
         String query = ("SELECT song.song_id, song.title, song.release_date, song.runtime "
                 + "FROM album_contains_song "
                 + "INNER JOIN song on album_contains_song.song_id = song.song_id "
@@ -189,14 +194,14 @@ public class AlbumService {
                 + "WHERE album.album_id=%d").formatted(albumId);
         Connection conn = DataSourceUtils.getConnection(dataSource);
 
-        //
+        //Try Catch Error Checking if song in album exists
         try{
             Statement stmt = conn.createStatement(
                     ResultSet.TYPE_SCROLL_INSENSITIVE,
                     ResultSet.CONCUR_UPDATABLE);
             ResultSet rs = stmt.executeQuery(query);
 
-        //
+        //If song does not exist in album it creates the song and adds it
             while(rs.next()) {
                 Song song = new Song();
                 song.setSongId(rs.getLong("song_id"));
@@ -218,10 +223,10 @@ public class AlbumService {
     }
 
     /**
-     *
-     * @param albumId
-     * @param trackNumber
-     * @return
+     * Gets the song in the specfic album through searching for its tracking number.
+     * @param albumId ID of Album
+     * @param trackNumber the tracking number for the Album
+     * @return the song in  Album
      */
     public Song getSongInAlbumByTrackNumber(long albumId, int trackNumber) {
         //
@@ -232,7 +237,7 @@ public class AlbumService {
                 + "WHERE album.album_id=%d AND album_contains_song.track_number=%d").formatted(albumId, trackNumber);
         Connection conn = DataSourceUtils.getConnection(dataSource);
 
-        //
+        //Try Catch Error Checking if song can be searched by its tracking number
         try{
             Statement stmt = conn.createStatement(
                     ResultSet.TYPE_SCROLL_INSENSITIVE,
@@ -241,7 +246,7 @@ public class AlbumService {
 
             Song song = new Song();
 
-            //
+            //Gets the song ID,Title,ReleaseDate and Runtime
             while(rs.next()) {
                 song.setSongId(rs.getLong("song_id"));
                 song.setTitle(rs.getString("title"));
@@ -264,18 +269,18 @@ public class AlbumService {
     // UPDATE
 
     /**
-     *
-     * @param albumId
-     * @param albumDetails
-     * @return
+     * Updates the Album id and details
+     * @param albumId ID of Album
+     * @param albumDetails Details of the Album
+     * @return -1
      */
     public int updateAlbum(long albumId, Album albumDetails) {
-        //
+        //String that prints a message to update the album id and details
         String st = ("UPDATE album SET title='%s', release_date='%tF' WHERE album_id=%d")
                 .formatted(albumDetails.getTitle(), albumDetails.getReleaseDate(), albumId);
         Connection conn = DataSourceUtils.getConnection(dataSource);
 
-        //
+        //Try Catch Error Checking if album can be updated
         try {
             Statement stmt = conn.createStatement(
                     ResultSet.TYPE_SCROLL_INSENSITIVE,
@@ -302,12 +307,12 @@ public class AlbumService {
      * @return
      */
     public int updateSongTrackNumberInAlbum(long albumId, long songId, int trackNumber) {
-        //
+        //String that prints a message to update a song by tracking number
         String st = ("UPDATE album_contains_song SET track_number=%d WHERE (album_id=%d AND song_id=%d)")
                 .formatted(trackNumber, albumId, songId);
         Connection conn = DataSourceUtils.getConnection(dataSource);
 
-        //
+        //Try Catch Error Checking if song can be updated by tracking number
         try {
             Statement stmt = conn.createStatement(
                     ResultSet.TYPE_SCROLL_INSENSITIVE,
@@ -328,16 +333,16 @@ public class AlbumService {
     // DELETE
 
     /**
-     *
-     * @param albumId
-     * @return
+     * Deletes album within the database.
+     * @param albumId ID of an album
+     * @return -1
      */
     public int deleteAlbum(long albumId) {
-        //
+        // String that prints a message to insert a song in albumdelete album
         String st = ("DELETE FROM album WHERE album_id=%d").formatted(albumId);
         Connection conn = DataSourceUtils.getConnection(dataSource);
 
-        //
+        // Try Catch Error Checking if album can be deleted
         try {
             Statement stmt = conn.createStatement(
                     ResultSet.TYPE_SCROLL_INSENSITIVE,
@@ -356,17 +361,17 @@ public class AlbumService {
     }
 
     /**
-     *
-     * @param albumId
-     * @param songId
-     * @return
+     * Deletes Album that has a specific song
+     * @param albumId ID of album
+     * @param songId ID of song
+     * @return -1
      */
     public int deleteAlbumContainsSong(long albumId, long songId){
-        //
+        //String that prints a message to delete an album with a specific song
         String st = ("DELETE FROM album_contains_song WHERE (album_id=%d AND song_id=%d)").formatted(albumId, songId);
         Connection conn = DataSourceUtils.getConnection(dataSource);
 
-        //
+        // Try Catch Error Checking if album can be deleted based on the song the user does not want
         try {
             Statement stmt = conn.createStatement(
                     ResultSet.TYPE_SCROLL_INSENSITIVE,
@@ -385,13 +390,13 @@ public class AlbumService {
     }
 
     /**
-     *
-     * @param collectionId
-     * @return
+     * Gets a list of Collection of Albums
+     * @param collectionId ID of collection
+     * @return list of a collection of albums
      */
     public List<Album> getCollectionAlbums(long collectionId)
     {
-        //
+        // String that prints a message to get a collection of songs
         String stmt = "SELECT album.album_id,album.title,album.release_date\n" +
                 "FROM album,collection_holds_album\n" +
                 "WHERE album.album_id=collection_holds_album.album_id\n" +
@@ -399,14 +404,14 @@ public class AlbumService {
         Connection conn = DataSourceUtils.getConnection(dataSource);
         List<Album> albums = new ArrayList<>();
 
-        //
+        // Try Catch Error Checking if albums can be recieved
         try {
             Statement statement = conn.createStatement(
                     ResultSet.TYPE_SCROLL_INSENSITIVE,
                     ResultSet.CONCUR_UPDATABLE);
             ResultSet rs = statement.executeQuery(stmt);
 
-            //
+            //If album does not exist - creates Album and then sets its ID, Title, ReleaseDate and album
             while(rs.next())
             {
                 Album album = new Album();
@@ -429,14 +434,15 @@ public class AlbumService {
     }
 
     /**
-     *
-     * @param albumId
-     * @return
+     * Recieves total rutime for the Album
+     * @param albumId ID of Album
+     * @return -1
      */
     public int getTotalRuntimeOfAlbum(long albumId)
     {
         int i = 0;
-        //
+
+        // String that prints a message to get total runtime for the album
         String stmt = "SELECT sum(song.runtime) AS total_runtime\n" +
                 "FROM song, album_contains_song,album\n" +
                 "WHERE album_contains_song.song_id=song.song_id\n" +
@@ -444,7 +450,7 @@ public class AlbumService {
                 "AND album.album_id=%d".formatted(albumId);
         Connection conn = DataSourceUtils.getConnection(dataSource);
 
-        //
+        // Try Catch Error Checking if total run time exists
         try {
             Statement statement = conn.createStatement(
                     ResultSet.TYPE_SCROLL_INSENSITIVE,
